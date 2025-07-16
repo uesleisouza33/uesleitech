586ec11802fc7a1857666ec18c7bbbfc8d5c8f5a
Ueslei Souza de Carvalho Filho <ucarvalhof@gmail.com>
2025-07-16 08:44:00 -0300

    Atualiza componentes e Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
-export default function Home() {
+import Link from "next/link";
+import { Metadata } from "next";
+import { Card } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+
+
+export const metadata: Metadata = {
+  title: "UesleiTech - Artigos sobre Finanças e Tecnologia",
+  description: "Explore artigos sobre programação, inteligência artificial, dólar, bitcoin e muito mais."
+};
+
+async function getPosts() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10", {
+    next: { revalidate: 60 },
+  });
+  return res.json();
+
+}
+
+export default async function Home() {
+  const posts = await getPosts();
   return (
-    <>
-      
-    </>
+    <main className="max-w-4xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Últimos artigos</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {posts.map((post: any) =>
+          <Card key={post.id} className="p-4">
+            <h2 className="text-2xl font-medium mb-2">{post.title}</h2>
+            <p className="text-sm text-gray-600 mb-4">{post.body.slice(0, 50)}...</p>
+            <Link href={`artigo/${post.id}`}>
+              <Button variant={"default"}>Leia mais</Button>
+            </Link>
+          </Card>
+        )}
+      </div>
+    </main>
   )
 }
