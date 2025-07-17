7575f8d4df63e58a5ba339478d28e174bab731d3
Ueslei Souza de Carvalho Filho <ucarvalhof@gmail.com>
2025-07-17 09:38:31 -0300

    Adiciona botão de voltar para home

--- app/artigo/[id]/page.tsx
+++ app/artigo/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
-import { title } from "process";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 async function getPost(id: number) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
@@ -31,6 +32,7 @@ export default async function Artigo({ params }: { params: { id: string } }) {
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
       <p>{post.body}</p>
+      <Button variant={"default"}><Link href={"/"}>Voltar</Link></Button>
     </div>
   );
 }
